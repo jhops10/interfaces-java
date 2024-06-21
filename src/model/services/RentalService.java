eea918085fc0467b408fd749488f13fd7c649541
João Paulo <joaowais@gmail.com>
2024-06-21 19:38:20 -0300

    Solução do problema - PARTE 2 (sem interface)

---
+++ src/model/services/RentalService.java
@@ -0,0 +1,59 @@
+package model.services;
+
+import model.entities.CarRental;
+import model.entities.Invoice;
+
+import java.time.Duration;
+
+public class RentalService {
+    private Double pricePerDay;
+    private Double pricePerHour;
+    private BrazilTaxService taxService;
+
+
+    public RentalService(Double pricePerDay, Double pricePerHour, BrazilTaxService taxService) {
+        this.pricePerDay = pricePerDay;
+        this.pricePerHour = pricePerHour;
+        this.taxService = taxService;
+    }
+
+    public Double getPricePerDay() {
+        return pricePerDay;
+    }
+
+    public void setPricePerDay(Double pricePerDay) {
+        this.pricePerDay = pricePerDay;
+    }
+
+    public Double getPricePerHour() {
+        return pricePerHour;
+    }
+
+    public void setPricePerHour(Double pricePerHour) {
+        this.pricePerHour = pricePerHour;
+    }
+
+    public BrazilTaxService getTaxService() {
+        return taxService;
+    }
+
+    public void setTaxService(BrazilTaxService taxService) {
+        this.taxService = taxService;
+    }
+
+    public void processInvoice(CarRental carRental) {
+        double minutes = Duration.between(carRental.getStart(), carRental.getFinish()).toMinutes();
+        double hours = Math.ceil(minutes / 60);
+        double basicPayment;
+
+        if (hours <= 12.0){
+            basicPayment = pricePerHour * hours;
+        } else {
+            basicPayment = pricePerDay * (hours /24);
+        }
+
+        double tax = taxService.tax(basicPayment);
+
+        carRental.setInvoice(new Invoice(basicPayment, tax));
+    }
+}
