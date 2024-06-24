385236a8f9b84ab02f86c01b3bdb9aa5c2d15ac5
João Paulo <joaowais@gmail.com>
2024-06-23 22:41:44 -0300

    Herdar vs Cumprir Contrato

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,46 +1,22 @@
 package application;
 
-import model.entities.CarRental;
-import model.entities.Vehicle;
-import model.services.BrazilTaxService;
-import model.services.RentalService;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Locale;
-import java.util.Scanner;
+import model.entities.AbstractShape;
+import model.entities.Circle;
+import model.entities.Rectangle;
+import model.entities.Shape;
+import model.enums.Color;
 
 public class Program {
     public static void main(String[] args) {
-        Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner(System.in);
-        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 
-        System.out.println("Entre com os dados do aluguel");
-        System.out.print("Modelo do Carro: ");
-        String carModel = sc.nextLine();
+        AbstractShape s1 = new Circle(Color.BLACK, 2.0);
+        AbstractShape s2 = new Rectangle(Color.WHITE, 3.0, 4.0);
 
-        System.out.print("Retirada (dd/MM/yyyy hh:mm):");
-        LocalDateTime start = LocalDateTime.parse(sc.nextLine(), fmt);
-        System.out.print("Retorno (dd/MM/yyyy hh:mm):");
-        LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
+        System.out.println("Circle Color:  " + s1.getColor());
+        System.out.println("Circle Area: " + String.format("%.3f",s1.area()));
 
-        CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
+        System.out.println("Rectangle Color: " + s2.getColor());
+        System.out.println("Rectangle Area: " + String.format("%.3f", s2.area()));
 
-        System.out.print("Entre com o preço por hora: ");
-        double pricePerHour = sc.nextDouble();
-        System.out.print("Entre com o preço por dia: ");
-        double pricePerDay = sc.nextDouble();
-
-        RentalService rentalService = new RentalService(pricePerDay, pricePerHour, new BrazilTaxService());
-
-        rentalService.processInvoice(cr);
-
-        System.out.println("FATURA: ");
-        System.out.println("Pagamento Básico: " + String.format("%.2f", cr.getInvoice().getBasicPayment()));
-        System.out.println("Imposto: " + String.format("%.2f", cr.getInvoice().getTax()));
-        System.out.println("Pagamento Total: " + String.format("%.2f", cr.getInvoice().getTotalPayment()));
-
-        sc.close();
     }
 }
